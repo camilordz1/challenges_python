7466b8c79d4d713bcc36e9cb874037b235a35246
Camilo Andres Rodriguez <andres.roh@gmail.com>
2023-03-14 12:53:05 -0500

    Add pentagonal numbers

---
+++ challenges/Intermediate/10-pentagonal_numbers.py
@@ -0,0 +1,31 @@
+# Pentagonal Number
+
+# Write a function that takes a positive integer num and calculates how many dots exist 
+# in a pentagonal shape around the center dot on the Nth iteration.
+
+# In the image below you can see the first iteration is only a single dot. On the second, 
+# there are 6 dots. On the third, there are 16 dots, and on the fourth there are 31 dots.
+
+# view imagen:
+# https://edabit-challenges.s3.amazonaws.com/pentagonal_number.png
+
+# Return the number of dots that exist in the whole pentagon on the Nth iteration.
+
+# Examples
+# pentagonal(1) ➞ 1
+
+# pentagonal(2) ➞ 6
+
+# pentagonal(3) ➞ 16
+
+# pentagonal(8) ➞ 141
+
+
+def pentagonal(num):
+	if num > 1:	
+		return pentagonal(num-1)+5*(num-1)
+	return 1
+
+
+if __name__ == "__main__":
+    print(pentagonal(8))	
